c153105410d24ca42b883c7e36098c8225e20481
Xuxiaoyu05 <34875319+Xuxiaoyu05@users.noreply.github.com>
2019-03-12 18:10:28 +0800

    Update 2. Add Two Numbers.py

--- 2. Add Two Numbers.py
+++ 2. Add Two Numbers.py
@@ -9,4 +9,50 @@
 # 输出：7 -> 0 -> 8
 # 原因：342 + 465 = 807
 
-class ListNode
+class ListNode:
+  def __init__(self, x):
+    self.val = x
+    self.next = None
+    
+class Solution:
+  def addTwoNumbers(self, l1, l2):   
+    dummy = ListNode(-1)  # 新建一个链表
+    pre = dummy
+
+    cur1, cur2 = l1, l2
+    add1 = 0
+   
+    while cur1 or cur2:
+      if cur1 and cur2:
+        temp = cur1.val + cur2.val + add1
+        cur1 = cur1.next
+        cur2 = cur2.next
+      elif cur1:
+        temp = cur1.val + add1
+        cur1 = cur1.next
+      else:
+        temp = cur2.val + add1
+        cur2 = cur2.val
+      
+      add1 = temp // 10
+      node = ListNode(temp % 10)
+      pre.next = node
+      pre = pre.next
+      
+    return dummy.next
+
+S = Solution()
+
+node1 = ListNode(2)
+node2 = ListNode(4)
+node3 = ListNode(3)
+node1.next = node2
+node2.next = node3
+
+node4 = ListNode(5)
+node5 = ListNode(6)
+node6 = ListNode(4)
+node4.next = node5
+node5.next = node6
+
+print(S.addTwoNumbers(node1, node4).val)
