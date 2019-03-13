bef18c2fb5c51efac41fa96d242dc4dde3a40e27
Xuxiaoyu05 <34875319+Xuxiaoyu05@users.noreply.github.com>
2019-03-13 10:09:37 +0800

    Update 61. Rotate List.py

--- 61. Rotate List.py
+++ 61. Rotate List.py
@@ -14,9 +14,47 @@ class Solution:
     if not head or k <= 0:
       return
     
-    dummy = ListNode(-1)
-    dummy.next = head
-    pre = dummy
+    length = 1
+    cur = head
+    while cur.next:
+      cur = cur.next
+      length += 1
     
+    cur.next = head  # 首尾相连
+   
+    gap = length - k % length    # 判断需要移动的次数
     
+    # cur = head   # 并不需要这一步，cur还是位于初始的尾结点
     
+    # 寻找待断开的前驱
+    while gap != 0:  
+      cur = cur.next  
+      gap -= 1
+    
+    head = cur.next  # 注意：此句话要加上
+    cur.next = None  # 将环链断开
+    
+    return head
+  
+  
+S = Solution()
+
+node1 = ListNode(1)
+
+node2 = ListNode(2)
+
+node3 = ListNode(3)
+
+node4 = ListNode(4)
+
+node5 = ListNode(5)
+
+node1.next = node2
+
+node2.next = node3
+
+node3.next = node4
+
+node4.next = node5
+
+print(S.rotateRight(node1, 2).next.val)
