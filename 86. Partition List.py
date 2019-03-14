16c9f5a7842939cb11c4fbe202ebca2ac502d235
Xuxiaoyu05 <34875319+Xuxiaoyu05@users.noreply.github.com>
2019-03-14 14:45:55 +0800

    Update 86. Partition List.py

--- 86. Partition List.py
+++ 86. Partition List.py
@@ -11,14 +11,37 @@ class ListNode:
 
 class Solution:
    def partition(self, head, x):
-      dummy = ListNode(-1)
-      dummy.next = head
-      pre = dummy
-      cur = head
-      while cur:
-        if cur.val >= 3:
-          
+      # 拆分成两个链表
+      first = ListNode(-1)
+      second = ListNode(-1)
+      cur1, cur2 = first, second
+      while head:
+        if head.val < x:
+          cur1.next = head
+          head = head.next
+          cur1 = cur1.next
         else:
-          pre = pre.next
-          cur = cur.next
+          cur2.next = head
+          head = head.next
+          cur2 = cur2.next
           
+       cur1.next = second.next
+       cur2.next = None
+       return first.next
+          
+S = Solution()
+
+node1 = ListNode(1)
+node2 = ListNode(4)
+node3 = ListNode(3)
+node4 = ListNode(2)
+node5 = ListNode(5)
+node6 = ListNode(2)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+node5.next = node6
+
+print(S.partition(node1, 3).next.next.next.val)         
